7d9e354e3f3d7e4bb9b41040034668509cb4fdf3
Sawix <13964975+Sawix@users.noreply.github.com>
2021-09-12 16:33:34 -0400

    Initial commit

---
+++ template.py
@@ -0,0 +1,35 @@
+"""
+Author: Jacob Dachenhaus, jdachenh@purdue.edu
+Assignment: m.n - Assignment Name
+Date: MM/DD/YYYY
+
+Description:
+    Describe your program here.
+
+Contributors:
+    None
+
+My contributor(s) helped me:
+    [x] understand the assignment expectations without
+        telling me how they will approach it.
+    [x] understand different ways to think about a solution
+        without helping me plan my solution.
+    [x] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor.
+
+Academic Integrity Statement:
+    I have not used source code obtained from any unauthorized
+    source, either modified or unmodified; nor have I provided
+    another student access to my code.  The project I am
+    submitting is my own original work.
+"""
+
+
+def main():
+    # TODO: Write code
+    exit(1)
+
+if __name__ == '__main__':
+    main()
