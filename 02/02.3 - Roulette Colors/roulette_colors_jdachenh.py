3a76c4ce3c1c0e0f6a3d98a8ec1a152ebf8546af
Jacob Dachenhaus <13964975+Sawix@users.noreply.github.com>
2021-09-20 21:35:58 -0400

    02.3 - Roulette Colors

---
+++ 02/02.3 - Roulette Colors/roulette_colors_jdachenh.py
@@ -0,0 +1,60 @@
+"""
+Author: Jacob Dachenhaus, jdachenh@purdue.edu
+Assignment: 02.3 - Roulette Colors
+Date: 09/20/2021
+
+Description:
+    Program to determine the color of roulette pockets by number.
+    Takes the following inputs:
+    -   The pocket number
+
+Contributors:
+    None
+
+My contributor(s) helped me:
+    [x] understand the assignment expectations without
+        telling me how they will approach it.
+    [x] understand different ways to think about a solution
+        without helping me plan my solution.
+    [x] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor.
+
+Academic Integrity Statement:
+    I have not used source code obtained from any unauthorized
+    source, either modified or unmodified; nor have I provided
+    another student access to my code.  The project I am
+    submitting is my own original work.
+"""
+
+
+def main():
+    # Take each input and cast to appropriate type
+    num = int(input("Please choose a pocket number: "))
+
+    # Validate inputs
+    if (num < 0) or (num > 36):
+        print("  Invalid Input!")
+        return
+
+    # Determine the color
+    if (num == 0):
+        color = "green"
+    elif (1 <= num <= 10) or (19 <= num <= 28):
+        if (num % 2 > 0):
+            color = "red"
+        else:
+            color = "black"
+    else:
+        if (num % 2 > 0):
+            color = "black"
+        else:
+            color = "red"
+
+    # Format data to match the sample
+    print(f"  Pocket number {num} is {color}.")
+
+
+if __name__ == '__main__':
+    main()
